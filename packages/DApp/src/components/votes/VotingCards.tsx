fbc28fbad0b5fd53275620bb1139bc095eb36522
Maria Rushkova <66270386+mrushkova@users.noreply.github.com>
2021-07-16 15:39:27 +0200

    Add empty state for searching (#128)

--- packages/DApp/src/components/votes/VotingCards.tsx
+++ packages/DApp/src/components/votes/VotingCards.tsx
@@ -54,7 +54,12 @@ export function VotingCards() {
         }
       })}
       {roomsToShow.length === 0 && empty && <VotingEmpty />}
-      {roomsToShow.length === 0 && !empty && <div>Community not found</div>}
+      {roomsToShow.length === 0 && !empty && (
+        <SearchEmpty>
+          <p>🔎</p>
+          <span>Vote not found</span>
+        </SearchEmpty>
+      )}
     </div>
   )
 }
@@ -104,5 +109,26 @@ const VoteType = styled(ButtonPrimary)`
   &.notSelected {
     background: none;
     color: ${Colors.VioletDark};
+  }
+`
+
+const SearchEmpty = styled.div`
+  padding: 0 32px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin-top: 90px;
+  background: ${Colors.White};
+  font-size: 22px;
+  line-height: 38px;
+  z-index: 99;
+
+  & > p {
+    font-weight: bold;
+    font-size: 64px;
+    line-height: 64%;
+    margin-bottom: 24px;
   }
 `
