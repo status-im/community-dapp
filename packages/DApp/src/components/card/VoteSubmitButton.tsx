3cc6f6873c51b54930b6bc98eeb9dff8dee92fcd
Szymon Szlachtowicz <38212223+Szymx95@users.noreply.github.com>
2021-07-30 08:45:49 +0200

    Show number of votes (#172)

--- packages/DApp/src/components/card/VoteSubmitButton.tsx
+++ packages/DApp/src/components/card/VoteSubmitButton.tsx
@@ -4,6 +4,8 @@ import { useContracts } from '../../hooks/useContracts'
 import { useVotesAggregate } from '../../hooks/useVotesAggregate'
 import { CurrentVoting } from '../../models/community'
 import { VoteSendingBtn } from '../Button'
+import { BigNumber } from 'ethers'
+import { addCommas } from '../../helpers/addCommas'
 
 interface VoteSubmitButtonProps {
   vote: CurrentVoting
@@ -13,8 +15,11 @@ export function VoteSubmitButton({ vote }: VoteSubmitButtonProps) {
   const { votes } = useVotesAggregate(vote.ID)
   const { votingContract } = useContracts()
   const { send } = useContractFunction(votingContract, 'castVotes')
+  const voteAmount = votes.reduce((prev, curr) => prev.add(curr[2]), BigNumber.from(0))
   if (votes.length > 0) {
-    return <VoteSendingBtn onClick={() => send(votes)}> {votes.length} votes need saving</VoteSendingBtn>
+    return (
+      <VoteSendingBtn onClick={() => send(votes)}> {addCommas(voteAmount.toString())} votes need saving</VoteSendingBtn>
+    )
   }
   return null
 }
