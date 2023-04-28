71da72fd145892edf3dcfa8ccbdf9f681e1c4aa3
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2023-04-28 18:49:30 +0200

    use correct dapp config

--- packages/DApp/src/index.tsx
+++ packages/DApp/src/index.tsx
@@ -3,46 +3,23 @@ import 'core-js/proposals/promise-any'
 import React from 'react'
 import { render } from 'react-dom'
 import { App } from './App'
-import { DAppProvider, ChainId } from '@usedapp/core'
-import { DEFAULT_CONFIG } from '@usedapp/core/dist/cjs/src/model/config/default'
-import { ConfigProvider } from './providers/config/provider'
+import { DAppProvider } from '@usedapp/core'
 import { WakuProvider } from './providers/waku/provider'
 import { CommunitiesProvider } from './providers/communities/provider'
 import { WakuFeatureProvider } from './providers/wakuFeature/provider'
-import { contracts, CustomChainId, getDAppConfig } from './providers/config/config'
-
-const config = {
-  readOnlyChainId: getDAppConfig(process.env.ENV).defaultChainId,
-  readonlyUrls: {
-    [ChainId.Ropsten]: 'https://ropsten.infura.io/v3/b4451d780cc64a078ccf2181e872cfcf',
-    [CustomChainId.OptimismGoerli]: 'https://optimism-goerli.infura.io/v3/4c90c025caca4de2b1419633554e6bca',
-  },
-  multicallAddresses: {
-    ...DEFAULT_CONFIG.multicallAddresses,
-    [ChainId.Ropsten]: contracts[ChainId.Ropsten].multicallContract,
-    [CustomChainId.OptimismGoerli]: contracts[CustomChainId.OptimismGoerli].multicallContract,
-    [ChainId.Hardhat]: contracts[ChainId.Hardhat].multicallContract,
-  },
-
-  notifications: {
-    checkInterval: 500,
-    expirationPeriod: 50000,
-  },
-}
+import { config } from './config'
 
 render(
   <React.StrictMode>
-    <ConfigProvider>
-      <WakuProvider>
-        <DAppProvider config={config}>
-          <CommunitiesProvider>
-            <WakuFeatureProvider>
-              <App />
-            </WakuFeatureProvider>
-          </CommunitiesProvider>
-        </DAppProvider>
-      </WakuProvider>
-    </ConfigProvider>
+    <WakuProvider>
+      <DAppProvider config={config.daapConfig}>
+        <CommunitiesProvider>
+          <WakuFeatureProvider>
+            <App />
+          </WakuFeatureProvider>
+        </CommunitiesProvider>
+      </DAppProvider>
+    </WakuProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
