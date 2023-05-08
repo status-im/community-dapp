25a4366fa23165ad01c8905f20419aa015466af9
Jakub Kotula <520927+jkbktl@users.noreply.github.com>
2023-05-08 09:52:41 +0200

    featured

--- packages/contracts/abi/index.ts
+++ packages/contracts/abi/index.ts
@@ -1,7 +1,8 @@
 import VotingContract from './../artifacts/contracts/VotingContract.sol/VotingContract.json'
 import Directory from './../artifacts/contracts/Directory.sol/Directory.json'
+import FeaturedVotingContract from './../artifacts/contracts/FeaturedVotingContract.sol/FeaturedVotingContract.json'
 import ERC20 from './ERC20.json'
 import ERC20Mock from './ERC20Mock.json'
 import MultiCall from './MultiCall.json'
 
-export { VotingContract, Directory, ERC20, MultiCall, ERC20Mock }
+export { VotingContract, Directory, FeaturedVotingContract, ERC20, MultiCall, ERC20Mock }
