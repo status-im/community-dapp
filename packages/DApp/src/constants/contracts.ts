032b30cd9c5dcf9d5e797ae8f80937e4a368fcf3
Felicio Mununga <felicio@users.noreply.github.com>
2023-11-02 13:03:05 +0100

    config testnet

--- packages/DApp/src/constants/contracts.ts
+++ packages/DApp/src/constants/contracts.ts
@@ -8,6 +8,13 @@ export const contracts = {
     directoryContract: '0xA8d270048a086F5807A8dc0a9ae0e96280C41e3A',
     tokenContract: '0x650AF3C15AF43dcB218406d30784416D64Cfb6B2',
   },
+  [ChainId.OptimismGoerli]: {
+    votingContract: '0x744Fd6e98dad09Fb8CCF530B5aBd32B56D64943b',
+    featuredVotingContract: '0x744Fd6e98dad09Fb8CCF530B5aBd32B56D64943b',
+    directoryContract: '0xB3Ef5B0825D5f665bE14394eea41E684CE96A4c5',
+    tokenContract: '0xcAD273fA2bb77875333439FDf4417D995159c3E1',
+    multicallContract: '0xcA11bde05977b3631167028862bE2a173976CA11',
+  },
   [ChainId.Hardhat]: {
     votingContract: process.env.VOTING_CONTRACT ?? '0x0000000000000000000000000000000000000000',
     directoryContract: process.env.DIRECTORY_CONTRACT ?? '0x0000000000000000000000000000000000000000',
