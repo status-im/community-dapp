d5db47c52151c71315e90a193bb7045a9a5a38d9
Felicio Mununga <felicio@users.noreply.github.com>
2023-11-02 13:16:07 +0100

    config testnet (#98)
    * config testnet
    
    * f
    
    (cherry picked from commit 55deba520ce004248c09aa6cc58d3b6b49c40b1f)

--- packages/DApp/src/constants/contracts.ts
+++ packages/DApp/src/constants/contracts.ts
@@ -7,6 +7,14 @@ export const contracts = {
     featuredVotingContract: '0x2EA9700E7F27E09F254f2DaEc5E05015b2b961d0',
     directoryContract: '0xA8d270048a086F5807A8dc0a9ae0e96280C41e3A',
     tokenContract: '0x650AF3C15AF43dcB218406d30784416D64Cfb6B2',
+    multicallContract: '0xeAa6877139d436Dc6d1f75F3aF15B74662617B2C',
+  },
+  [ChainId.OptimismGoerli]: {
+    votingContract: '0x744Fd6e98dad09Fb8CCF530B5aBd32B56D64943b',
+    featuredVotingContract: '0x744Fd6e98dad09Fb8CCF530B5aBd32B56D64943b',
+    directoryContract: '0xB3Ef5B0825D5f665bE14394eea41E684CE96A4c5',
+    tokenContract: '0xcAD273fA2bb77875333439FDf4417D995159c3E1',
+    multicallContract: '0xcA11bde05977b3631167028862bE2a173976CA11',
   },
   [ChainId.Hardhat]: {
     votingContract: process.env.VOTING_CONTRACT ?? '0x0000000000000000000000000000000000000000',
